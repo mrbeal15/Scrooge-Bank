140535787e2ea537332a02cc6ad667074f15e201
Matt Beal <mrbeal15@gmail.com>
2025-10-31 11:12:42 -0500

    set up health check endpoint and test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  healthCheck(): { status: string, message: string } {
+    return { status: '200OK', message: 'healthy' };
   }
 }
